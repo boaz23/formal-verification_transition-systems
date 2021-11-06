f52631ca144d8d18b6c03f2c38dba8d4d877852e
boaz23 <boaz23mw@gmail.com>
2021-11-06 10:30:35 +0200

    Assignment 2 - another test
    * Added a test which tests for a situation where some states are
      unreachable and expects them not to show up in the resulting
      transition system states.

--- assignment2_tests.py
+++ assignment2_tests.py
@@ -61,9 +61,85 @@ def test2():
     assert ts == expected
 
 
+def test3():
+    ts = convertCircuit(1, 2, 1, lambda s: ((s[2] and s[1]) ^ s[0], s[2] ^ s[1]) if s != (True, False, True) else (False, True), lambda s: (s[0] and (s[1] or s[2]),))
+    expected = {
+        'AP': {'r1', 'r2', 'x1', 'y1'},
+        'Act': {(False,), (True,)},
+        'I': {(False, False, False), (False, False, True)},
+        'L': {
+            (False, False, False): set(),
+            (False, False, True): {'x1'},
+            (False, True, False): {'r2'},
+            (False, True, True): {'r2', 'x1'},
+            (True, False, False): {'r1'},
+            (True, False, True): {'r1', 'x1', 'y1'}
+        },
+        'S': {
+            (False, False, False),
+            (False, False, True),
+            (False, True, False),
+            (False, True, True),
+            (True, False, False),
+            (True, False, True)
+        },
+        'to': {
+            ((False, False, False), (False,), (False, False, False)),
+            ((False, False, False), (True,), (False, False, True)),
+            ((False, False, True), (False,), (False, True, False)),
+            ((False, False, True), (True,), (False, True, True)),
+            ((False, True, False), (False,), (False, True, False)),
+            ((False, True, False), (True,), (False, True, True)),
+            ((False, True, True), (False,), (True, False, False)),
+            ((False, True, True), (True,), (True, False, True)),
+            ((True, False, False), (False,), (True, False, False)),
+            ((True, False, False), (True,), (True, False, True)),
+            ((True, False, True), (False,), (False, True, False)),
+            ((True, False, True), (True,), (False, True, True))
+        }
+    }
+
+    assert ts == expected
+
+
+def test4():
+    ts = convertCircuit(1, 2, 1, lambda s: (True, True), lambda s: (s[2] ^ s[1] ^ s[0],))
+    expected = {
+        'AP': {'r1', 'r2', 'x1', 'y1'},
+        'Act': {(False,), (True,)},
+        'I': {(False, False, False), (False, False, True)},
+        'L': {
+            (False, False, False): set(),
+            (False, False, True): {'x1', 'y1'},
+            (True, True, False): {'r1', 'r2'},
+            (True, True, True): {'r1', 'r2', 'x1', 'y1'}
+        },
+        'S': {
+            (False, False, False),
+            (False, False, True),
+            (True, True, False),
+            (True, True, True)
+        },
+        'to': {
+            ((False, False, False), (False,), (True, True, False)),
+            ((False, False, False), (True,), (True, True, True)),
+            ((False, False, True), (False,), (True, True, False)),
+            ((False, False, True), (True,), (True, True, True)),
+            ((True, True, False), (False,), (True, True, False)),
+            ((True, True, False), (True,), (True, True, True)),
+            ((True, True, True), (False,), (True, True, False)),
+            ((True, True, True), (True,), (True, True, True))
+        }
+    }
+
+    assert ts == expected
+
+
 def runTests():
     test1()
     test2()
+    test3()
+    test4()
 
 
 if __name__ == '__main__':
