6ee01461af8a6a7fcebb938cda400d5795b1ba90
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2021-11-16 17:15:17 +0200

    Added question 4 from theoretic assignment

--- assignment3_tests.py
+++ assignment3_tests.py
@@ -227,8 +227,96 @@ def test_example():
     }
     assert ts == expected
 
+
+def test_petarson():
+    eval_dict = {
+        'true': lambda eta: True,
+        'start_condition': lambda eta: eta['y0'] == 0 and eta['y1'] == 0 and eta['s'] == 1,
+        'c_enter0': lambda eta: eta['y1'] == 0 or eta['s'] != 0,
+        'c_enter1': lambda eta: eta['y0'] == 0 or eta['s'] != 1,
+    }
+    effect_dict = {
+        'nothing': lambda eta: eta,
+        'doorway0': lambda eta: {**eta, 'y0': 1, 's': 0},
+        'doorway1': lambda eta: {**eta, 'y1': 1, 's': 1},
+        'reset0': lambda eta: {**eta, 'y0': 0},
+        'reset1': lambda eta: {**eta, 'y1': 0},
+    }
+
+    def evaluate(cond, eta):
+        return eval_dict[cond](eta)
+
+    def effect(act, eta):
+        return effect_dict[act](eta)
+
+    pg = {
+        'Loc': {
+            'l_nc0, l_nc1',
+            'l_nc0, l_w1',
+            'l_nc0, l_cr1',
+            'l_w0, l_nc1',
+            'l_w0, l_w1',
+            'l_w0, l_cr1',
+            'l_cr0, l_nc1',
+            'l_cr0, l_w1',
+            'l_cr0, l_cr1',
+        },
+        'Loc0': {'l_nc0, l_nc1'},
+        'Act': {'nothing', 'doorway0', 'doorway1', 'reset0', 'reset1'},
+        'Eval': evaluate,
+        'Effect': effect,
+        'to': {
+            ('l_nc0, l_nc1', 'true', 'doorway0', 'l_w0, l_nc1'),
+            ('l_nc0, l_nc1', 'true', 'doorway1', 'l_nc0, l_w1'),
+            ('l_nc0, l_w1', 'true', 'doorway0', 'l_w0, l_w1'),
+            ('l_nc0, l_w1', 'c_enter1', 'nothing', 'l_nc0, l_cr1'),
+            ('l_nc0, l_cr1', 'true', 'doorway0', 'l_w0, l_cr1'),
+            ('l_nc0, l_cr1', 'true', 'reset1', 'l_nc0, l_nc1'),
+            ('l_w0, l_nc1', 'c_enter0', 'nothing', 'l_cr0, l_nc1'),
+            ('l_w0, l_nc1', 'true', 'doorway1', 'l_w0, l_w1'),
+            ('l_w0, l_w1', 'c_enter0', 'nothing', 'l_cr0, l_w1'),
+            ('l_w0, l_w1', 'c_enter1', 'nothing', 'l_w0, l_cr1'),
+            ('l_w0, l_cr1', 'c_enter0', 'nothing', 'l_cr0, l_cr1'),
+            ('l_w0, l_cr1', 'true', 'reset1', 'l_w0, l_nc1'),
+            ('l_cr0, l_nc1', 'true', 'reset0', 'l_nc0, l_nc1'),
+            ('l_cr0, l_nc1', 'true', 'doorway1', 'l_cr0, l_w1'),
+            ('l_cr0, l_w1', 'true', 'reset0', 'l_nc0, l_w1'),
+            ('l_cr0, l_w1', 'c_enter1', 'nothing', 'l_cr0, l_cr1'),
+            ('l_cr0, l_cr1', 'true', 'reset0', 'l_nc0, l_cr1'),
+            ('l_cr0, l_cr1', 'true', 'reset1', 'l_cr0, l_nc1'),
+        },
+        'g0': 'start_condition',
+    }
+
+    vars = {'y0': range(0, 2), 'y1': range(0, 2), 's': range(0, 2)}
+    labels = {'c_enter0', 'c_enter1'}
+
+    ts = transitionSystemFromProgramGraph(
+        pg=pg,
+        vars=vars,
+        labels=labels,
+        labelsIsMap=True,
+    )
+
+    print('digraph G {')
+    print('node [shape = point ]; qi')
+    print('node [shape = box]; ')
+    for s in ts['I']:
+        print(f'qi -> "{s[0]} {s[1]["y0"]},{s[1]["y1"]},{s[1]["s"]}";')
+
+    for (s, a, ss) in ts['to']:
+        print(f'"{s[0]} {s[1]["y0"]},{s[1]["y1"]},{s[1]["s"]}"', "->",
+              f'"{ss[0]} {ss[1]["y0"]},{ss[1]["y1"]},{ss[1]["s"]}"', f"[label={a}];")
+
+    print('}')
+
+    sortComponents(ts)
+    pprint(ts)
+
+
 def runTests():
     test_example()
+    test_petarson()
 
 
 if __name__ == '__main__':
